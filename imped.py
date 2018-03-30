bug fix of pressure calculation

imped.py
@@ -179,7 +179,9 @@ def calc_transmission(wf,men):
             men.tm[1,0] = 1j*s1*np.sin(x)/_rhoc0
     else:
         # length 0
-        men.tm = np.eye(2, dtype=complex)
+        # men.tm is initialized by Identity matrix
+        # men.tm = np.eye(2, dtype=complex)
+        pass 
     
 def calc_impedance(wf, men):
     '''calculate impedance and other data for a given mensur cell'''

xmensur.py
@@ -24,7 +24,7 @@ class Men(object):
         self.c_ratio = c_ratio # child connection ratio
         # below are calculated later
         # for impedance
-        self.tm = np.zeros((2,2),dtype = complex) # transmission matrix
+        self.tm = np.eye(2,dtype = complex) # transmission matrix, default Identity
         self.zi = 0 # impedance at input end
         self.zo = 0 # impedance at output end
         # for pressure 